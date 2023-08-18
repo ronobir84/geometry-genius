f8eb313b8fda5b8219d5d9bc5798131cda7dd9d4
ronodir84 <priashdas@84gmail.com>
2023-08-18 17:55:48 +0600

    this is add the all section

--- script/area-calculate.js
+++ script/area-calculate.js
@@ -38,3 +38,33 @@ function calculateRectangleArea(){
     rectangleAreaSpan.innerText = area;
     
 }
+//reusable function --> reduce duplicate code
+function calculateParallelogramArea(){
+    const base = getInputValue('Parallelogram-base')
+    console.log(base)
+    const height = getInputValue('Parallelogram-height')
+    console.log(height)
+    const area = base * height;
+    setElementInnerText('Parallelogram-area', area)
+    
+}
+function calculateEllipseArea(){
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const minorRadius = getInputValue('ellipse-minor-radius')
+    const area = 3.14 * majorRadius * minorRadius;
+    setElementInnerText('ellipse-area', area);
+}
+//reusable get input value field in number
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+
+//reusable set span , p, div, etc, text
+function setElementInnerText(elementId ,area){
+    const element = document.getElementById(elementId)
+    element.innerText = area;
+
+}
